3aff3aa4d7bb3e31a170f560d0fd9518e7007e11
msynko <marina.synko@gmail.com>
2019-04-01 23:23:01 -0400

    added coresponded props and displayed API call data

--- src/App.js
+++ src/App.js
@@ -6,21 +6,52 @@ import Display from './Display/Display';
 
 const API_key = "3b9d55c144c787244255479141b68ee4";
 
+
 class App extends Component {
 
-  loadData = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${API_key}`);
+state = {
+  temperature: undefined,
+  city: undefined,
+  country: undefined,
+  humidity: undefined,
+  description: undefined,
+  error: undefined
+
+}
+
+  getData = async (e) => {
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
     const response = await api_call.json();
     console.log(response);
-    
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      error: ""
+    })
       }
+      // eslint-disable-next-line no-use-before-define
   render() {
     return (
 
         <div>
             <Poster />
-            <SearchBar />
-            <Display />
+            <SearchBar 
+            loadData = {this.getData}
+            />
+            <Display 
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}/>
         </div>
       
     );
