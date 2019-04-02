3ed279f3379ea1c3b8d7e9aefea61c576b0418e5
msynko <marina.synko@gmail.com>
2019-04-01 22:00:27 -0400

    added input fields and created api call

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Poster from './Poster/Poster';
 import SearchBar from './SearchBar/SearchBar';
 import Display from './Display/Display';
 
+const API_key = "3b9d55c144c787244255479141b68ee4";
+
 class App extends Component {
+
+  loadData = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${API_key}`);
+    const response = await api_call.json();
+    console.log(response);
+    
+      }
   render() {
     return (
-     
+
         <div>
             <Poster />
             <SearchBar />
             <Display />
-
         </div>
       
     );
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const SearchBar = (props) =>{
   return (
-    <div>
-       <form onSubmit={props.loadData}">
-            <input type="text" name="city" placeholder="city"/>
-            <input type="text" name="country" placeholder="country"/>
-          <input type="submit" value="Submit"/>
-        </form>
-    </div>
+     
+    <form onSubmit={props.loadData}>
+        <input type="text" name="city" placeholder="city" />
+        <input type="text" name="country" placeholder="country" />
+    <button>Get Weather</button>
+  </form>
+    
     )
 }
 export default SearchBar;
